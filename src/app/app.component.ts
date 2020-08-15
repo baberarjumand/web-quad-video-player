d7b3e23fe07227a142a49848cac122ab036dea7e
baberarjumand <baber.arjumand@gmail.com>
2020-08-15 20:56:44 +0300

    testing key handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -78,15 +78,15 @@ export class AppComponent implements OnInit {
     vid4Node.src = fileUrl;
   }
 
-  // vid1arrowUp() {
-  //   console.log('Vid 1 Arrow Up');
-  // }
+  vid1arrowUp() {
+    console.log('Vid 1 Arrow Up');
+  }
 
-  // vid1arrowDown() {
-  //   console.log('Vid 1 Arrow Down');
-  // }
+  vid1arrowDown() {
+    console.log('Vid 1 Arrow Down');
+  }
 
-  // vid1RightClick() {
-  //   console.log('Vid 1 Right Click');
-  // }
+  vid1RightClick() {
+    console.log('Vid 1 Right Click');
+  }
 }
