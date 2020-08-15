1248c0e921c180901934380095684ce43d95a5f4
baberarjumand <baber.arjumand@gmail.com>
2020-08-15 20:51:02 +0300

    removed unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -78,19 +78,19 @@ export class AppComponent implements OnInit {
     vid4Node.src = fileUrl;
   }
 
-  vid1arrowUp() {
-    console.log('Vid 1 Arrow Up');
-  }
+  // vid1arrowUp() {
+  //   console.log('Vid 1 Arrow Up');
+  // }
 
-  vid1arrowDown() {
-    console.log('Vid 1 Arrow Down');
-  }
+  // vid1arrowDown() {
+  //   console.log('Vid 1 Arrow Down');
+  // }
 
-  vid1RightClick() {
-    console.log('Vid 1 Right Click');
-  }
+  // vid1RightClick() {
+  //   console.log('Vid 1 Right Click');
+  // }
 
-  vid1DblClick() {
-    console.log('Vid 1 Double Click');
-  }
+  // vid1DblClick() {
+  //   console.log('Vid 1 Double Click');
+  // }
 }
